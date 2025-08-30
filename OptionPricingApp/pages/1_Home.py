9487d9363ed3ba6f1b79ff87a0a6b4944e1c0648
Takshotty <takeshichang2028@u.northwestern.edu>
2025-08-29 21:18:47 -0700

    Update 1_Home.py

--- OptionPricingApp/pages/1_Home.py
+++ OptionPricingApp/pages/1_Home.py
@@ -3,13 +3,13 @@ import streamlit as st
 st.title("Tak's Simple Option Pricer")
 
 st.write("""
-This web app demonstrates two fundamental option pricing models:
-- **Black–Scholes Model**
-- **Binomial Tree Model**
-
-and also includes:
-- **Visualizations**
-- **About Me**
+This web app demonstrates two fundamental option pricing models: 
+- The **Black–Scholes** Model
+    - Calculates option prices using a formula based on stock price, time, volatility, and interest rates.
+    - Best for European options that can only be exercised at expiration.
+- The **Binomial Tree** Model
+    - Estimates option prices by simulating step-by-step stock price movements in a tree.
+    - Flexible for European and American options that can be exercised early.
 
 Use the sidebar to navigate between pages. Enjoy!
 """)
